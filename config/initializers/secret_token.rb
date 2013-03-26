cf1a4a6199c6251023de0c51a5ee453c3d638dad
rootinflux <rootinflux@rootinfluxvm.(none)>
2013-03-26 14:13:25 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstapp::Application.config.secret_token = '59ec6f98878a2c1ba7cf50ff4eb40ce663729a165f4836def8bc9651d75130e356138127abd7bb13e2ff94332073f9ae4744649d09887d8b06c9b4ef8646a32c'
